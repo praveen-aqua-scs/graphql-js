33bf9f41b36c542b5a9ac5031bfd96001611c238
praveensirvi-aqua <147171704+praveensirvi-aqua@users.noreply.github.com>
2025-09-22 20:27:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,39 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "East US"
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "example-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+# 1️⃣ Secret in custom_data
 resource "azurerm_virtual_machine" "example_vm" {
   name                  = "example-vm"
   location              = azurerm_resource_group.example.location
@@ -22,15 +58,41 @@ resource "azurerm_virtual_machine" "example_vm" {
   os_profile {
     computer_name  = "hostname"
     admin_username = "adminuser"
-    
-    # 🔐 Hardcoded secret in custom_data (this will be detected by Aqua)
+    admin_password = "VMAdminSecret123!" # 🔐 May also be detected
     custom_data = <<-EOF
       #!/bin/bash
-      echo "MyAppPassword=HardcodedSecretAquaShouldCatch123#" >> /etc/environment
+      echo "DB_PASSWORD=SuperSecretInCustomData456!" >> /etc/environment
     EOF
   }
 
   os_profile_linux_config {
     disable_password_authentication = false
   }
+}
+
+# 2️⃣ Secret in container environment variable
+resource "azurerm_container_group" "example" {
+  name                = "example-containergroup"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  os_type             = "Linux"
+
+  container {
+    name   = "appcontainer"
+    image  = "nginx:latest"
+    cpu    = "0.5"
+    memory = "1.5"
+
+    environment_variables = {
+      "APP_SECRET" = "EnvSecretAquaShouldDetect789#"
+    }
+
+    ports {
+      port     = 80
+      protocol = "TCP"
+    }
+  }
+
+  ip_address_type = "public"
+  dns_name_label  = "example-containergroup"
 }
